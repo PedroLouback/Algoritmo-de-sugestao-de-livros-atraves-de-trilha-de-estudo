0f6db0e4ef64d7ead74d2c7237f57195870f6a9b
PedroLouback <pcampos952@gmail.com>
2022-11-12 22:19:23 -0300

    Código desenvolvido para de aprendizado

---
+++ main.py
@@ -0,0 +1,9 @@
+import networkx as nx
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+G = nx.read_adjlist('files/adjacency_list.txt', delimiter="  ", create_using=nx.Graph(), nodetype=str)
+
+nx.draw(G, with_labels=True, font_color='black', edgecolors='red', node_color='lightgray', node_size=1000)
+plt.show()
