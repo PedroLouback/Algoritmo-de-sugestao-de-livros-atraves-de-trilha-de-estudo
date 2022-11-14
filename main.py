32a132e7716e85d1b95f7dec7407d3230c22a3e3
PedroLouback <pcampos952@gmail.com>
2022-11-14 19:46:09 -0300

    ambiente virtual cirado

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
-G = nx.read_adjlist('files/adjacency_list.txt', delimiter="  ", create_using=nx.Graph(), nodetype=str)
+G = nx.read_adjlist('files/adjacency_list.csv', delimiter=";", create_using=nx.Graph(), nodetype=str)
 
 nx.draw(G, with_labels=True, font_color='black', edgecolors='red', node_color='lightgray', node_size=1000)
 plt.show()
