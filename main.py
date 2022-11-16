ad0c929f4487f02f458e7b6791693490b1f668d7
PedroLouback <pcampos952@gmail.com>
2022-11-15 22:56:31 -0300

    Tentativa de adicionar e imprimir pesos

--- main.py
+++ main.py
@@ -5,17 +5,5 @@ import matplotlib.pyplot as plt
 
 G = nx.read_adjlist('files/adjacency_list.csv', delimiter=";", create_using=nx.Graph(), nodetype=str)
 
-print('Opções de estudo: ')
-print(G.nodes())
-input()
-
-option = 'Sim'
-
-while option == 'Sim' or option == 'sim':
-    theme = input('Informe o tema que deseja introduzir seu estudo: ')
-    for v in G.nodes():
-        if v == theme:
-            print(v)
-
-nx.draw(G, with_labels=True, font_color='black', node_color='red', node_size=1200)
+nx.draw(G, with_labels=True, font_color='black', edgecolors='red', node_color='lightgray', node_size=1000)
 plt.show()
